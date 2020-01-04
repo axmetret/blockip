bcd8b2f36966577bb28b02838e4df5761545361f
axmetret <axmetret@gmail.com>
2020-01-04 21:01:54 +1000

    fix code and del addrules.sh

--- addrules.sh
+++
@@ -1,3 +0,0 @@
-# Adding a logging rule to determine the network scanner
-iptables -I INPUT 1 -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -j LOG --log-level=1 --log-prefix "Ping request: "
-
